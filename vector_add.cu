fb4c68dcb1f8032b1558dab57ac24c8cca0df9be
harish786786786 <harish25120013@snuchennai.edu.in>
2026-03-04 16:45:56 +0530

    Add files via upload

---
+++ vector_add.cu
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define N 8
+
+// -----------------------------
+// GPU Kernel Function
+// -----------------------------
+__global__ void vectorAdd(int *A, int *B, int *C)
+{
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(id < N)
+    {
+        C[id] = A[id] + B[id];
+    }
+}
+
+// -----------------------------
+// Host Function (CPU)
+// -----------------------------
+int main()
+{
+    int A[N], B[N], C[N];
+
+    int *d_A, *d_B, *d_C;
+
+    int size = N * sizeof(int);
+
+    // Initialize vectors
+    for(int i=0;i<N;i++)
+    {
+        A[i] = i;
+        B[i] = i*2;
+    }
+
+    // Allocate GPU memory
+    cudaMalloc((void**)&d_A,size);
+    cudaMalloc((void**)&d_B,size);
+    cudaMalloc((void**)&d_C,size);
+
+    // Copy data CPU → GPU
+    cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
+
+    // Kernel launch
+    vectorAdd<<<1,N>>>(d_A,d_B,d_C);
+
+    // Copy result GPU → CPU
+    cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
+
+    printf("Vector Addition Result:\n");
+
+    for(int i=0;i<N;i++)
+    printf("%d ",C[i]);
+
+    printf("\n");
+
+    // Free GPU memory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    return 0;
+}
